49865354073a3c70520304a212159fe40de0a58a
estebanfern <estebangfernandeza@gmail.com>
2022-12-25 13:12:26 -0300

    Correccion de bugs

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const doc = document.getElementById("nrodoc");
 const tipo = document.getElementById("tipodoc");
 
-doc.addEventListener('keypress', (e) =>{
+doc.addEventListener('keyup', (e) =>{
     if (tipo.value == 1){
         verificarString(e.key);
     }
@@ -12,18 +12,43 @@ tipo.addEventListener('change', () => {
 });
 
 function verificarString(car) {
-    if ((car >=0 || car <= 9) || (car == '-')){
-        if (doc.value.indexOf('-') >= 0) {
-            if (car == '-'){
-                restarChar();
-            }else if (doc.value.indexOf('-') == doc.value.length-2) {
+    if (car == '-'){
+        if (doc.value.charAt(0) == '-'){
+            doc.value = "";
+        }else{
+            if (contarGuiones() > 1){
                 restarChar();
             }
         }
+    }else if (car >= 0 && car <= 9){
+        if (contarGuiones() == 1 && doc.value.charAt(doc.value.length - 3) == '-'){
+            restarChar();
+        }
+    }else if (!(car == 'Backspace' || car == 'Enter')){
+        restarChar();
     }
+    formatear();
+}
+
+function contarGuiones(){
+    let i = 0;
+    for (let k = 0; k < doc.value.length; k++){
+        if (doc.value.charAt(k) == '-'){
+            i++;
+        }
+    }
+    return i;
 }
 
 function restarChar() {
-    doc.value = doc.value.substr(0, doc.value.indexOf('-'))
-    console.log("hecho");
+    doc.value = doc.value.substring(0, doc.value.length - 1);
+}
+
+function formatear(){
+    let ind = doc.value.indexOf('-');
+    let corte = 0;
+    if (ind != -1){
+        corte = ind < doc.value.length - 1 ? ind + 2 : ind + 1;
+        doc.value = doc.value.substring(0, corte); 
+    }
 }
